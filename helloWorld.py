9680b5b338897ff308372fd90be3c132c8fb59bb
bhigley <bhigley@zagmail.gonzaga.edu>
2022-01-17 13:17:25 -0800

    can count words in a file

--- helloWorld.py
+++ helloWorld.py
@@ -2,18 +2,27 @@
 # def main():
 # list.count(x) returns number of times x is in list
 
+STOP_WORDS = ['a', 'an', 'and', 'are', 'as', 'be', 'by', 
+'for', 'if', 'in', 'is', 'it', 'of', 
+'or', 'py', 'rst', 'that', 'the', 'to', 'with',]
+
+
 def count_words(curr_list, list_index):
     #check_counted_words(word)
     
     word = curr_list[list_index]
     num_word = curr_list.count(word)
-    #print(word)
-    #print(num_word)
+
     return word, num_word
 
 # checks that the current word hasn't already been counted
 def check_counted_words(curr_word, words_list):
-    print("test")
+    for word in words_list: # iterates through list
+       if curr_word == word:
+           #print(curr_word, word)
+           return True
+        
+    return False
 
 def get_word_list(file_name):
     words_list = []
@@ -27,7 +36,7 @@ def get_word_list(file_name):
 
 
 def main():
-    # a list that stores each wrod from a txt file
+    # a list that stores each word from a txt file
     words_list = []
     counted_words_list =[] # Keeps track of words already counted
     final_word_list = [] # holds the final list of counted words
@@ -35,13 +44,18 @@ def main():
     words_list = get_word_list('text2.txt')
 
     list_index = 0;
-   # for word in words_list:
-   #     count_words(words_list, list_index)
-    #    list_index = list_index + 1
 
     for word in words_list:
-        final_word_list.append(count_words(words_list, list_index))
-        list_index = list_index + 1
+        already_counted = check_counted_words(words_list[list_index], counted_words_list)
+        if already_counted == False:
+            final_word_list.append(count_words(words_list, list_index))
+            # updates list of already counted words
+            counted_words_list.append(words_list[list_index])
+            list_index = list_index + 1
+        else:
+            list_index = list_index + 1
+
+        already_counted = True
 
     print(final_word_list)   
     print('Test2')
